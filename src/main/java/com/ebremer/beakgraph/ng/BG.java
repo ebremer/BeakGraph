d3e32a788f252d34c8ff4ce53ecdd3544b0f8e5b
Erich Bremer <erich@ebremer.com>
2024-01-31 16:22:42 -0500

    wip

--- src/main/java/com/ebremer/beakgraph/ng/BG.java
+++ src/main/java/com/ebremer/beakgraph/ng/BG.java
@@ -15,7 +15,6 @@ public class BG {
     private static final Model m = ModelFactory.createDefaultModel();
     public static final String NS = "https://www.ebremer.com/beakgraph/ns/";
     public static final Resource BeakGraph = m.createResource(NS+"BeakGraph");
-    public static final Resource BeakDataSet = m.createResource(NS+"BeakDataSet");
     public static final Resource PredicateVector = m.createResource(NS+"PredicateVector");
     public static final Resource Dictionary = m.createResource(NS+"Dictionary");
     public static final Resource NamedGraphs = m.createResource(NS+"NamedGraphs");
