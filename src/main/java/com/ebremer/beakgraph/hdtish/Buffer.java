9a55d0d9a27cf54f8a913ed9297095226c6a6412
Erich Bremer <erich@ebremer.com>
2025-04-28 00:11:16 -0400

    wip

--- src/main/java/com/ebremer/beakgraph/hdtish/Buffer.java
+++ src/main/java/com/ebremer/beakgraph/hdtish/Buffer.java
@@ -7,55 +7,55 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Buffer {
-    private ByteArrayOutputStream stream;
+    private ByteArrayOutputStream buffer;
     private List<EntryMetadata> entries;
 
     public Buffer() {
-        stream = new ByteArrayOutputStream();
+        buffer = new ByteArrayOutputStream();
         entries = new ArrayList<>();
     }
 
     public void add(String string) {
-        int startPosition = stream.size();
+        int startPosition = buffer.size();
         byte[] bytes = string.getBytes(StandardCharsets.UTF_8);
-        stream.write(bytes, 0, bytes.length);
+        buffer.write(bytes, 0, bytes.length);
         entries.add(new EntryMetadata(startPosition, DataType.STRING));
     }
 
     public void add(Float f) {
-        int startPosition = stream.size();
+        int startPosition = buffer.size();
         ByteBuffer buffer = ByteBuffer.allocate(Float.BYTES);
         buffer.putFloat(f);
-        stream.write(buffer.array(), 0, Float.BYTES);
+        this.buffer.write(buffer.array(), 0, Float.BYTES);
         entries.add(new EntryMetadata(startPosition, DataType.FLOAT));
     }
 
     public void add(Double d) {
-        int startPosition = stream.size();
+        int startPosition = buffer.size();
         ByteBuffer buffer = ByteBuffer.allocate(Double.BYTES);
         buffer.putDouble(d);
-        stream.write(buffer.array(), 0, Double.BYTES);
+        this.buffer.write(buffer.array(), 0, Double.BYTES);
         entries.add(new EntryMetadata(startPosition, DataType.DOUBLE));
     }
 
     public void add(int n) {
-        int startPosition = stream.size();
+        int startPosition = buffer.size();
         ByteBuffer buffer = ByteBuffer.allocate(Integer.BYTES);
         buffer.putInt(n);
-        stream.write(buffer.array(), 0, Integer.BYTES);
+        this.buffer.write(buffer.array(), 0, Integer.BYTES);
         entries.add(new EntryMetadata(startPosition, DataType.INTEGER));
     }
 
     public void add(long n) {
-        int startPosition = stream.size();
+        int startPosition = buffer.size();
         ByteBuffer buffer = ByteBuffer.allocate(Long.BYTES);
         buffer.putLong(n);
-        stream.write(buffer.array(), 0, Long.BYTES);
+        this.buffer.write(buffer.array(), 0, Long.BYTES);
         entries.add(new EntryMetadata(startPosition, DataType.LONG));
     }
 
     public int size() {
-        return stream.size();
+        return buffer.size();
     }
 
     private enum DataType {
